d77fb89bb5150112cff151da01ca548b015b4ebe
frmnsyhxixi <93929002+frmnsyhxixi@users.noreply.github.com>
2021-12-18 09:45:46 +0700

    update index.js

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ kurr.on('group-participants-update', async (anu) => {
 
 				img = await getBuffer(ppUrl)
 
-				teks = `ðŸŒ¹ Hi @${num.split('@')[0]} ðŸ‘‹\nðŸŒ¹ Selamat Datang Di Group: ${mdata.subject}\n\n Join Pada Jam: ${time} Untuk Menampilkan Menu Silahkan Ketik .Menu`
+				teks = `✅ Hi @${num.split('@')[0]} ✅\nSelamat Datang Di Group: ${mdata.subject}\n\n Join Pada Jam: ${time} Untuk Menampilkan Menu Silahkan Ketik .Menu`
 
 				sendButImage(anu.jid, teks, `Byy Store`, img,but = [{buttonId:`menu`, 
 
