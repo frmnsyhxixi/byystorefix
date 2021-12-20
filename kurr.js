777a5812104acd18ba3487e652062792bdf2820c
frmnsyhxixi <93929002+frmnsyhxixi@users.noreply.github.com>
2021-12-20 11:32:39 +0700

    update kurr.js

--- kurr.js
+++ kurr.js
@@ -1972,7 +1972,7 @@ runtime = process.uptime()
 Contoh = .menu
 
 `
-sendButLocation(from, `${menu}`, "*_© ByyStore_*", {jpegThumbnail:ofrply1,name:""}, [{buttonId:`${prefix}list`,buttonText:{displayText:'LIST HARGA'},type:1},{buttonId:`${prefix}caraorder`,buttonText:{displayText:'CARA ORDER'},type:1}], {contextInfo: { mentionedJid: [dtod,otod,stod]}})
+sendButLocation(from, `${menu}`, "*_© ByyStore_*", {jpegThumbnail:ofrply1,name:""}, [{buttonId:`${prefix}pormat`,buttonText:{displayText:'FORMAT ORDER'},type:1},{buttonId:`${prefix}caraorder`,buttonText:{displayText:'CARA ORDER'},type:1}], {contextInfo: { mentionedJid: [dtod,otod,stod]}})
 }
 else if(menusimpel = true){
         stod = `${sender}`
